be294791d7da262c280aea54cdfb1c2a0d0d68d9
Egg <85519326+Egg55@users.noreply.github.com>
2022-12-23 16:24:45 -0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,31 @@
+
+def food(food_name,ingr='',diet='',health='',cuisine_type='',type='')
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+    require 'json'
+
+    app_key = '' #enter your app key
+    app_id = '' #enter your app id
+
+    url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=#{food_name}&app_id=#{app_id}&app_key=#{app_key}#{ingr}#{diet}#{health}#{cuisine_type}#{type}&dishType=Soup&calories=100-1000&time=1-120&excluded=peanuts&random=true")
+  
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+  
+    if response.code == "200"
+      result = JSON.parse(response.body)
+
+      puts result['hits'][0]['recipe']['label']
+
+    else
+      puts "ERROR!!!: #{response.code} \n #{response}"
+    end
+end
+  
+
+food(food_name='fish',diet='&diet=low-fat')
